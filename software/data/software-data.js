6c8ffcf58a6b0aa05f1f57d257b16e8cf495ffd9
Amit Bhardwaj <96241046+Apeiro7@users.noreply.github.com>
2026-01-21 22:51:44 +0530

    Create software-data.js

---
+++ software/data/software-data.js
@@ -0,0 +1,20 @@
+const softwareList = [
+  {
+    name: "Microsoft Office 2024",
+    description: "Office 2024 Professional Plus for Windows 10/11",
+    image: "https://media.getintopc.com/2024/09/Microsoft-Office-Professional-Plus-2024-Free-Download.png",
+    link: "https://drive.google.com/file/d/11nS40I4_JHuPy3VFZTSZJRmLlBg2R5gl/view"
+  },
+  {
+    name: "DIA Windows (Official)",
+    description: "Official DIA Windows installer (Intel i3 or later)",
+    image: "https://delhiias.com/images/logo.png",
+    link: "https://d1p1719lr4ku8e.cloudfront.net/institute/delhiias/installer_files/windows/28a80bd16bf347f6ac420d31a5390c93.exe"
+  },
+  {
+    name: "DIA Windows (Pre-installed)",
+    description: "Pre-installed DIA package for Windows",
+    image: "https://delhiias.com/images/logo.png",
+    link: "https://ln5.sync.com/dl/bb0c57c60#fk3wpwct-cnecuse2-gczrht7j-kq96tyyf"
+  }
+];
